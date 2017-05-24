8aace184eae344a999ad6aa5887543dadbbb2d3b
Anton Krylov <anthony.kryloff@gmail.com>
2017-05-24 12:47:34 +0300

    fix configs

--- src/main/java/ru/kpfu/itis/aekrylov/itproject/WebConfig.java
+++ src/main/java/ru/kpfu/itis/aekrylov/itproject/WebConfig.java
@@ -1,24 +1,27 @@
-package ru.kpfu.itis.aekrylov.itproject.config;
+package ru.kpfu.itis.aekrylov.itproject;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
 import org.springframework.web.servlet.view.freemarker.FreeMarkerViewResolver;
 
-
 /**
  * By Anton Krylov (anthony.kryloff@gmail.com)
- * Date: 5/12/17 12:09 PM
+ * Date: 5/24/17 12:34 PM
  */
 
 @Configuration
 @ComponentScan(basePackageClasses = {ru.kpfu.itis.aekrylov.itproject.controllers.MainController.class})
 @EnableGlobalMethodSecurity(prePostEnabled = true, proxyTargetClass = true)
 @EnableWebSecurity
-public class DispatcherConfig {
+@EnableWebMvc
+public class WebConfig extends WebMvcConfigurerAdapter {
 
     @Bean
     public FreeMarkerConfigurer freeMarkerConfigurer() {
@@ -35,4 +38,10 @@ public class DispatcherConfig {
         resolver.setContentType("text/html;charset=UTF-8");
         return resolver;
     }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**").addResourceLocations("/static/");
+    }
+
 }
--- src/main/java/ru/kpfu/itis/aekrylov/itproject/WebInitializer.java
+++ src/main/java/ru/kpfu/itis/aekrylov/itproject/WebInitializer.java
@@ -1,14 +1,9 @@
 package ru.kpfu.itis.aekrylov.itproject;
 
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import ru.kpfu.itis.aekrylov.itproject.config.DispatcherConfig;
 import ru.kpfu.itis.aekrylov.itproject.security.WebSecurityConfig;
 
 import javax.servlet.ServletContext;
@@ -18,9 +13,7 @@ import javax.servlet.ServletRegistration;
  * By Anton Krylov (anthony.kryloff@gmail.com)
  * Date: 5/12/17 12:05 PM
  */
-@EnableWebMvc
-@Configuration
-public class WebInitializer extends WebMvcConfigurerAdapter implements WebApplicationInitializer {
+public class WebInitializer implements WebApplicationInitializer {
 
     @Override
     public void onStartup(ServletContext container) {
@@ -29,27 +22,23 @@ public class WebInitializer extends WebMvcConfigurerAdapter implements WebApplic
                 new AnnotationConfigWebApplicationContext();
         rootContext.register(ApplicationConfig.class);
         rootContext.register(WebSecurityConfig.class);
+        rootContext.register(WebConfig.class);
 
         // Manage the lifecycle of the root application context
         container.addListener(new ContextLoaderListener(rootContext));
 
         // Create the dispatcher servlet's Spring application context
+/*
         AnnotationConfigWebApplicationContext dispatcherContext =
                 new AnnotationConfigWebApplicationContext();
         dispatcherContext.register(DispatcherConfig.class);
+*/
 
         // Register and map the dispatcher servlet
         ServletRegistration.Dynamic dispatcher =
-                container.addServlet("dispatcher", new DispatcherServlet(dispatcherContext));
+                container.addServlet("dispatcher", new DispatcherServlet(rootContext));
         dispatcher.setLoadOnStartup(1);
         dispatcher.addMapping("/");
     }
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        super.addResourceHandlers(registry);
-        registry.addResourceHandler("/static/**").addResourceLocations("/static/");
-    }
-
 
 }
